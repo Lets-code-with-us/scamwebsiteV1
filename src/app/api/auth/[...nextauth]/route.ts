0bf7bdf74956a1f129ebefca05d2803ff5048e65
Shu12388y <spl.sp999@gmail.com>
2024-02-29 22:52:59 +0530

    file added

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth/next";
-import { option } from "./option" 
-
-const handler = NextAuth(option);
-
-
-export { handler as GET, handler as POST }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/component/NavBar";
 import { Footer } from "@/components/component/Footer";
-import AuthProvider from "@/components/component/authProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,14 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-
       <body className={inter.className}>
-        <NavBar/>
+        <NavBar />
         {children}
-        <Footer/>
-        </body>
-      </AuthProvider>
+        <Footer />
+      </body>
     </html>
   );
 }
