2dd0fd044c3ca675e29fa2eb0e000fe6a163a0f7
Shu12388y <spl.sp999@gmail.com>
2024-04-04 13:55:39 +0530

    file added

---
+++ src/app/api/user/getlikes/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest,NextResponse } from "next/server";
+import { dbConnect } from "@/db/dbConnect";
+import { Like } from "@/models/likeModel";
+
+
+dbConnect()
+export async function GET(request:NextRequest){
+try {
+    const response = await request.json();
+    const {blogId} = await response;
+    const getLike = await Like.find({blogId});
+    if(!getLike){
+        return NextResponse.json({"message":"server error"},{status:404})
+    }
+
+
+    return NextResponse.json({"messagge":"success","data":getLike},{status:200})
+
+
+} catch (error:any) {
+    throw new Error(error)
+}
+}
--- src/app/api/user/postlike/route.ts
+++ src/app/api/user/postlike/route.ts
@@ -1,20 +1,41 @@
-import { NextRequest,NextResponse } from "next/server";
-import {Like} from "@/models/likeModel"
+import { NextRequest, NextResponse } from "next/server";
+import { Like } from "@/models/likeModel";
 import { dbConnect } from "@/db/dbConnect";
+import JWT, { JwtPayload } from "jsonwebtoken";
 
-
-
-dbConnect()
-export async function POST(request:NextRequest) {
-try {
-    const token = request.cookies.get("token")?.value || ""
-    if(!token){
-        return NextResponse.json({"message":"Login First"},{status:404})
+dbConnect();
+export async function POST(request: NextRequest) {
+  try {
+    const response = await request.json();
+    const { blog, user } = await response;
+    const token = request.cookies.get("token")?.value || "";
+    if (!token) {
+      return NextResponse.json({ message: "Login First" }, { status: 404 });
     }
-    
-    
-} catch (error) {
-    
-}
-    
+    const decrypt: JwtPayload = JWT.verify(
+      token,
+      process.env.SECERT_KEY!
+    ) as JwtPayload;
+    const userId = decrypt.id;
+    const findUser = await Like.findById(userId);
+    const findBlog = await Like.findById(blog);
+    if (![findUser && findBlog]) {
+      const likeModel = await new Like({
+        userId,
+        blog,
+      });
+      const saveLikeModel = await likeModel.save();
+      if (!saveLikeModel) {
+        return NextResponse.json({ message: "error" }, { status: 404 });
+      }
+      return NextResponse.json({ message: "Like added" }, { status: 200 });
+    }
+
+    return NextResponse.json(
+      { message: "Like already added" },
+      { status: 404 }
+    );
+  } catch (error:any) {
+    throw new Error(error)
+  }
 }
