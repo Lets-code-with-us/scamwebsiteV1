747b37c0f39d85a668cd2f7af704fcca9f4b5484
Shub12388y <spl.sp999@gmail.com>
2024-10-11 21:32:51 +0530

    resolve function issue in profile function

--- src/app/api/user/profile/route.ts
+++ src/app/api/user/profile/route.ts
@@ -1,38 +1,39 @@
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import JWT, { JwtPayload } from "jsonwebtoken";
 import { dbConnect } from "@/db/dbConnect";
-import {User} from "@/models/userModel";
+import { User } from "@/models/userModel";
+import { Retina } from "@tsparticles/engine";
 
+dbConnect();
 
-
-dbConnect()
-
-export async function GET(request:NextRequest){
-
-    const getCookie = request.cookies.get('token')?.value || '';
-    if(!getCookie){
-        return NextResponse.json({"message":"Login required"},{status:404})
+export async function GET(request: NextRequest) {
+  try {
+    const getCookie = request.cookies.get("token")?.value || "";
+    if (!getCookie) {
+      return NextResponse.json({ message: "Login required" }, { status: 404 });
     }
 
-    const decrypt: JwtPayload = await JWT.verify(getCookie, process.env.SECERT_KEY!) as JwtPayload;
-        if(!decrypt){
-            return NextResponse.json({"message":'server error'},{status:500})
-        }
+    const decrypt: JwtPayload = (await JWT.verify(
+      getCookie,
+      process.env.SECERT_KEY!
+    )) as JwtPayload;
+    if (!decrypt) {
+      return NextResponse.json({ message: "server error" }, { status: 500 });
+    }
 
-        const findUser = await User.findById(decrypt.id)
+    const findUser = await User.findById(decrypt.id);
 
+    if (!findUser) {
+      return NextResponse.json({ message: "User not Found" }, { status: 404 });
+    }
 
-        if(!findUser){
-            return NextResponse.json({"message":"User not Found"},{status:404})
-        }
+    const getuser = await findUser.username;
 
-        const getuser = await findUser.username
-
-
-        return NextResponse.json({"message":"sucess","data":getuser},{status:200})
-
-
-
-
-
+    return NextResponse.json(
+      { message: "sucess", data: getuser },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({message:"Internal Server Error"},{status:500})
+  }
 }
