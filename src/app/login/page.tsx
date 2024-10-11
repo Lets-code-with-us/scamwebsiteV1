8418ddd564ca66c4847f672f78448cc61f6d138f
adilkadivala <adilkadivala560@gmail.com>
2024-10-11 21:27:10 +0530

    fixed #29 / completed Improve the UI/layout of login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -45,10 +45,10 @@ function Page() {
       <div>
         <Toaster />
       </div>
-      <section>
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-          <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
-            <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+      <section className="p-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+          <div className="flex items-center justify-center px-4 py-6 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+            <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md w-[75vw]">
               <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
                 Log in
               </h2>
--- src/components/component/Footer.tsx
+++ src/components/component/Footer.tsx
@@ -31,7 +31,7 @@ export function Footer() {
     
   }
   return (
-    <footer className="w-full">
+    <footer className="w-full px-3">
       <div>
         <Toaster/>
       </div>
